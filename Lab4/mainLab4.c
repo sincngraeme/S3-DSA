87949b701dc7e8dc584a7967ec1b27342304e62f
sincngraeme <166749247+sincngraeme@users.noreply.github.com>
2024-09-11 12:01:43 -0400

    completed lab 4

--- Lab4/mainLab4.c
+++ Lab4/mainLab4.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "crc.h"
 
 int main()
 {
-    // some code to do with crc
+    // Declare variables
+    char message[] = "123456789";       //message buffer
+    char* CRCstring;
+    crc compCRC;
+    int nBytes = strlen(message);
 
+    int newMessageSize = nBytes + 5 + 3;
+    char* newMessage = (char*)malloc(newMessageSize * sizeof(char));
+
+    compCRC = crcSlow(message, strlen(message));  //pointer to where the message starts
+
+    for (int i = 0; i <= nBytes; i++)
+    {
+        compCRC = crcSlow(message, nBytes);
+        sprintf(CRCstring, " 0x%x", compCRC);
+        strcpy(newMessage, (char*)message);
+        strcat(newMessage, CRCstring);
+        printf("sent message with CRC: %s\n", newMessage);
+        message[i] = '0';
+    }
+
+    free(newMessage);
     return 0;
 }
