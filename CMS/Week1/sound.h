c024bbc6888c73d2d22e7979568ba8e7919fe501
fcpage <158251261+fcpage@users.noreply.github.com>
2024-10-18 11:00:50 -0400

    commented on all files

--- CMS/Week1/sound.h
+++ CMS/Week1/sound.h
@@ -8,8 +8,8 @@
 #include <windows.h>					// Contains WAVEFORMATEX structure
 
 // CONSTANTS - SET RECORDING TIME AND RATE
-#define	DEFAULT_NSAMPLES	4000	
-#define MIN_BUFSIZE			1000
+#define	DEFAULT_NSAMPLES	4000	////Default number of samples.
+#define MIN_BUFSIZE			1000	////Minimum buffer size.
 #define RECORD_TIME			6		// seconds to record from mic
 #define SAMPLES_SEC			8000	// number of samples per second
 #define	NFREQUENCIES		96		// number of frequencies used - leave this alone
@@ -18,14 +18,14 @@
 // FUNCTION PROTOTYPES
 /* ************************ */
 // Playback
-int InitializePlayback(void);
-int PlayBuffer(short *piBuf, long lSamples);
-void ClosePlayback(void);
+int InitializePlayback(void);						////Sets up playback.
+int PlayBuffer(short *piBuf, long lSamples);		////Plays recorded audio through a buffer/output device.
+void ClosePlayback(void);							////Cleans out the buffers and closes the output device.
 
 // Recording
-int InitializeRecording(void);
-int	RecordBuffer(short *piBuf, long lBufSize);
-void CloseRecording(void);
+int InitializeRecording(void);						////Sets up recording.
+int	RecordBuffer(short *piBuf, long lBufSize);		////Records audio from input device to buffer.
+void CloseRecording(void);							////Cleans out the buffers and closes the input device.
 
 // Support functions for Playback functions (updated 2021)
 void SetupFormat(WAVEFORMATEX* wf);			// Used by InitializePlayback()					
