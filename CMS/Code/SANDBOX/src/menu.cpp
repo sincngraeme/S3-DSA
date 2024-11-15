8b874ace484b302e0bdd022ede8d86ffb35b6dd0
sincngraeme <166749247+sincngraeme@users.noreply.github.com>
2024-11-14 23:45:27 -0500

    dgjerkfnkrfn

--- CMS/Code/SANDBOX/src/menu.cpp
+++ CMS/Code/SANDBOX/src/menu.cpp
@@ -69,6 +69,7 @@ int TxMode()
                 if (getchar() == 'y')
                 {
                     TxAudio(soundObj.iBigBuf, soundObj.lBigBufSize);	
+                    getchar();
                 }
                 break;
             }
