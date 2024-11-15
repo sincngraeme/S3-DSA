2d1b4cebf035c783e30edb029e3eb6e34c98c61f
sincngraeme <166749247+sincngraeme@users.noreply.github.com>
2024-11-14 20:33:13 -0500

    changed timeouts

--- CMS/Code/SANDBOX/src/RS232Comm.cpp
+++ CMS/Code/SANDBOX/src/RS232Comm.cpp
@@ -165,11 +165,11 @@ int RS232Comm::SetComParms(HANDLE* hCom, int nComRate, int nComBits, COMMTIMEOUT
 
 	// Set communication timeouts (SEE COMMTIMEOUTS structure in MSDN) - want a fairly long timeout
 	memset((void *)&timeout, 0, sizeof(timeout));
-	timeout.ReadIntervalTimeout = MAXDWORD; 
+	timeout.ReadIntervalTimeout = 500; 
 	//500;					// Maximum time allowed to elapse before arival of next byte in milliseconds. If the interval between the arrival of any two bytes exceeds this amount, the ReadFile operation is completed and buffered data is returned
-	timeout.ReadTotalTimeoutMultiplier = 0; 
+	timeout.ReadTotalTimeoutMultiplier = 1; 
 	//1;			// The multiplier used to calculate the total time-out period for read operations in milliseconds. For each read operation this value is multiplied by the requested number of bytes to be read
-	timeout.ReadTotalTimeoutConstant = 0; 
+	timeout.ReadTotalTimeoutConstant = 5000; 
 	//5000;		// A constant added to the calculation of the total time-out period. This constant is added to the resulting product of the ReadTotalTimeoutMultiplier and the number of bytes (above).
 	SetCommTimeouts(*hCom, &timeout);
 	return RS232_NO_ERR;
